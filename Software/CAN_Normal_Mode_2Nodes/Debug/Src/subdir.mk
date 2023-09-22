9389bcce117ec52c0ca74ac4cd3d60b68c2a7928
Biitforce <biitforce24@gmail.com>
2023-09-22 11:52:56 +0530

    Nucleo board communication

---
+++ Software/CAN_Normal_Mode_2Nodes/Debug/Src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/it.c \
+../Src/main.c \
+../Src/msp.c \
+../Src/system_stm32f4xx.c 
+
+C_DEPS += \
+./Src/it.d \
+./Src/main.d \
+./Src/msp.d \
+./Src/system_stm32f4xx.d 
+
+OBJS += \
+./Src/it.o \
+./Src/main.o \
+./Src/msp.o \
+./Src/system_stm32f4xx.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -Og -ffunction-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/it.cyclo ./Src/it.d ./Src/it.o ./Src/it.su ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/msp.cyclo ./Src/msp.d ./Src/msp.o ./Src/msp.su ./Src/system_stm32f4xx.cyclo ./Src/system_stm32f4xx.d ./Src/system_stm32f4xx.o ./Src/system_stm32f4xx.su
+
+.PHONY: clean-Src
+
